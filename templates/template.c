dc78f59098e08bc2ba87a950baaab2605b7821b6
Kovid Goyal <kovid@kovidgoyal.net>
2012-08-13 12:28:36 +0530

    License on C source code changed to GPL3

--- templates/template.c
+++ templates/template.c
@@ -2,7 +2,7 @@
  * %FFILE%
  * Copyright (C) %YEAR% %USER% <%MAIL%>
  *
- * Distributed under terms of the MIT license.
+ * Distributed under terms of the GPL3 license.
  */
 
 #include "%FILE%.h"
--- templates/template.h
+++ templates/template.h
@@ -2,7 +2,7 @@
  * %FFILE%
  * Copyright (C) %YEAR% %USER% <%MAIL%>
  *
- * Distributed under terms of the MIT license.
+ * Distributed under terms of the GPL3 license.
  */
 
 #ifndef __%GUARD%__
