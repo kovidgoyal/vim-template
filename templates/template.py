0828e1de9bdcef045ca86eb555a21e6735aa6cc9
Kovid Goyal <kovid@kovidgoyal.net>
2013-04-10 11:05:42 +0530

    Update python template

--- templates/template.py
+++ templates/template.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
-# vim:fileencoding=UTF-8
+# vim:fileencoding=utf-8
 from __future__ import (unicode_literals, division, absolute_import,
                         print_function)
 
-__license__   = 'GPL v3'
+__license__ = 'GPL v3'
 __copyright__ = '%YEAR%, %USER% <%MAIL%>'
-__docformat__ = 'restructuredtext en'
 
 %HERE%
